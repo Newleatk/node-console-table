bfa491eed5e67e699e8a0e4cfeee974bfba370be
Benderwan Launay <benderwan.launay@newlea.tk>
2015-08-02 12:50:58 +0200

    Updating tests
    Updating test and base file to DEV

--- console-table.js
+++ console-table.js
@@ -1,3 +1,34 @@
-exports.init = function(){
-	console.log('Initialized');
-}
+exports.init = (function(options){
+	options = {padding: 1}
+	return {
+		data: function(data){
+			max = [];
+			for(row of data){
+				c = 0;
+				for(cell of row){
+					max[c] = max[c] > cell.c.length ? max[c] : cell.c.length;
+					c++; 
+				}
+			}
+			delete c;
+			for(row of data){
+				c = 0;
+				for(cell of row){
+					while(cell.c.length < max[c]){
+						cell.c = cell.c + " ";
+					}
+					c++; 
+				}
+				delete c;
+			}
+			w = max.length + 1 + (max.length * 2 * options.padding);
+			for(c of max){
+				w = w + c;
+			}
+			return {
+				render: function(){
+				}
+			}
+		}
+	}
+});
--- test.js
+++ test.js
@@ -1,2 +1,30 @@
 var options = {};
-var table = require('./console-table.js').init(options);
+var content = [
+	[
+		{
+			h: true,
+			c: "Bug ID"
+		},
+		{
+			h: true,
+			c: "Author"
+		}
+	],
+	[
+		{
+			c: "#3498"
+		},
+		{
+			c: "Benderwan"
+		}
+	],
+	[
+		{
+			c: "#3497"
+		},
+		{
+			c: "Tinouss"
+		}
+	]
+];
+var table = require('./console-table.js').init(options).data(content).render();
